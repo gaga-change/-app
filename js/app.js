143746b9b52a83b1b192ddb18285292d22d60efd
严俊东 <1172482914@qq.com>
2016-11-01 20:41:33 +0800

    显示数据

--- js/app.js
+++ js/app.js
@@ -4,57 +4,82 @@
 var app = angular.module('wyApp', [
     'ngRoute',
     'ngAnimate'
-]).config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
-
-}]).controller('wyCtrl', ['$scope', function ($scope) {
-    $scope.size = {
-        windowH: $(window).height(),
-        windowW: $(window).width()
-    };
-    $(window).resize(function () {
-        $scope.$apply(function () {
-            $scope.size = {
-                windowH: $(window).height(),
-                windowW: $(window).width()
-            };
+])
+    .config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
+        $locationProvider.hashPrefix('!');
+        $routeProvider
+            .when('/list/:listId', {
+                templateUrl: 'content/news-list.html',
+                controller: 'newListCtrl'
+            })
+            .otherwise('/list/jinxuan');
+    }])
+    .controller('wyCtrl', ['$scope', function ($scope) {
+        $scope.size = {
+            windowH: $(window).height(),
+            windowW: $(window).width()
+        };
+        $(window).resize(function () {
+            $scope.$apply(function () {
+                $scope.size = {
+                    windowH: $(window).height(),
+                    windowW: $(window).width()
+                };
+            })
+        });
+        $scope.component = {
+            header: 'header/header.html'
+        };
+        $scope.barList = [
+            // {name : '头条'},
+            {name: '精选', ename: 'jinxuan', url: 'T1467284926140'},
+            // {name: '娱乐', url: ''},
+            // {name: '娱乐'},
+            {name: '体育', ename: 'tiyu', url: 'T1348649079062'},
+            // {name: '网易号'},
+            // {name: '视屏'},
+            // {name: '财经'},
+            // {name: '科技'},
+            // {name: '汽车'},
+            {name: '时尚', ename: 'shishang', url: 'T1348650593803'},
+            // {name: '图片'},
+            // {name: '直播'},
+            // {name: '热点'},
+            // {name: '跟贴'},
+            // {name: '房产'},
+            // {name: '股票'},
+            {name: '轻松一刻', ename: 'qingsongyike', url: 'T1350383429665'},
+            // {name: '段子'},
+            // {name: '军事'},
+            {name: '历史', ename: 'lishi', url: 'T1368497029546'},
+            {name: '家居', ename: 'jiaju', url: 'T1348654105308'},
+            {name: '独家', ename: 'dujia', url: 'T1370583240249'},
+            // {name: '游戏'},
+            // {name: '健康'},
+            {name: '政务', ename: 'zhengwu', url: 'T1414142214384'},
+            {name: '漫画', ename: 'manhua', url: 'T1444270454635'},
+            {name: '达达趣闻', ename: 'dadaqvwen', url: 'T1444289532601'},
+            {name: '彩票', ename: 'caipiao', url: 'T1356600029035'}
+            // {name: '美女'},
+        ]
+    }])
+    .controller('newListCtrl', ['$scope', '$routeParams', '$rootScope', function ($scope, $routeParams, $rootScope) {
+        // $scope.param = $routeParams;
+        var news = getIndex($scope.barList, {ename: $routeParams.listId});
+        var url = "http://c.3g.163.com/nc/article/list/" + news.url + "/0-20.html";
+        console.log(url);
+        $scope.data = {};
+        $.post('http://localhost:8080/tools/jsonp', {url: url}, function (data) {
+            // console.log(data.data);
+            var d = data.data;
+            var list = JSON.parse(data.data)[news.url];
+            console.log(list);
+            $rootScope[news.ename] = list;
+            $scope.$apply(function () {
+               $scope.data = list;
+            })
         })
-    });
-    $scope.component = {
-        header: 'header/header.html'
-    };
-    $scope.barList = [
-        // {name : '头条'},
-        {name: '精选', url: 'T1467284926140'},
-        // {name: '娱乐', url: ''},
-        // {name: '娱乐'},
-        {name: '体育', url: 'T1348649079062'},
-        // {name: '网易号'},
-        // {name: '视屏'},
-        // {name: '财经'},
-        // {name: '科技'},
-        // {name: '汽车'},
-        {name: '时尚', url: 'T1348650593803'},
-        // {name: '图片'},
-        // {name: '直播'},
-        // {name: '热点'},
-        // {name: '跟贴'},
-        // {name: '房产'},
-        // {name: '股票'},
-        {name: '轻松一刻', url: 'T1350383429665'},
-        // {name: '段子'},
-        // {name: '军事'},
-        {name: '历史', url: 'T1368497029546'},
-        {name: '家居', url: 'T1348654105308'},
-        {name: '独家', url: 'T1370583240249'},
-        // {name: '游戏'},
-        // {name: '健康'},
-        {name: '政务', url: 'T1414142214384'},
-        {name: '漫画', url: 'T1444270454635'},
-        {name: '达达趣闻', url: 'T1444289532601'},
-        {name: '彩票', url: 'T1356600029035'}
-        // {name: '美女'},
-    ]
-}]);
+    }]);
 
 $(document).ready(function () {
     hideHeadTop();
@@ -77,3 +102,15 @@ $(document).ready(function () {
 
 
 });
+
+function getIndex(arr, obj) {
+    if(arr.length < 1)return {};
+    if(obj.length < 1)return {};
+    var re ;
+    for(var i = 0; i < arr.length; i++){
+        for(var j in obj){
+            if(obj[j] == arr[i][j])return arr[i];
+        }
+    }
+    return {};
+}
