4cb07c44df66ebae9eecc3438ff5d2ce7c6db41b
严俊东 <1172482914@qq.com>
2016-11-01 14:32:09 +0800

    做好框架

--- js/app.js
+++ js/app.js
@@ -5,21 +5,40 @@ var app = angular.module('wyApp', [
     'ngRoute',
     'ngAnimate',
     'wyApp.header'
-])
-    .config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
+]).config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
 
-    }])
-    .controller('wyCtrl', ['$scope', function ($scope) {
-        $scope.size = {
-            windowH: $(window).height(),
-            windowW: $(window).width()
-        };
-        $(window).resize(function () {
-            $scope.$apply(function () {
-                $scope.size = {
-                    windowH: $(window).height(),
-                    windowW: $(window).width()
-                };
-            })
+}]).controller('wyCtrl', ['$scope', function ($scope) {
+    $scope.size = {
+        windowH: $(window).height(),
+        windowW: $(window).width()
+    };
+    $(window).resize(function () {
+        $scope.$apply(function () {
+            $scope.size = {
+                windowH: $(window).height(),
+                windowW: $(window).width()
+            };
         })
-    }]);
+    })
+}]);
+
+$(document).ready(function () {
+    hideHeadTop();
+    $(window).scroll(hideHeadTop);
+    /**
+     * 自动隐藏头部图标
+     */
+    function hideHeadTop() {
+        var h = $('[data-id = header]');
+        /**
+         * 判断当前document.scrollTop
+         */
+        console.log($(document).scrollTop());
+        if($(document).scrollTop() > $(window).height()*0.75){
+
+        }else {
+
+        }
+    }
+
+});
