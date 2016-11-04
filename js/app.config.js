15c21ef52312c41ab5e6215b42bdc6cf24de73ad
严俊东 <1172482914@qq.com>
2016-11-04 17:20:26 +0800

    添加模块切换的动画

--- js/app.config.js
+++ js/app.config.js
@@ -10,6 +10,34 @@ app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $u
                 }
             }
         }
-    )
-    $urlRouterProvider.otherwise('index');
-}])
+    ).state('index.xinwen', {
+        url: "/xinwen",
+        views:{
+            contentSon:{
+                templateUrl: 'com/xinwen.html'
+            }
+        }
+    }).state('index.wo', {
+        url: "/wo",
+        views:{
+            contentSon:{
+                templateUrl: 'com/wo.html'
+            }
+        }
+    }).state('index.huati', {
+        url: "/huati",
+        views:{
+            contentSon:{
+                templateUrl: 'com/huati.html'
+            }
+        }
+    }).state('index.zhibo', {
+        url: "/zhibo",
+        views:{
+            contentSon:{
+                templateUrl: 'com/zhibo.html'
+            }
+        }
+    })
+    $urlRouterProvider.otherwise('index/xinwen');
+}]);
